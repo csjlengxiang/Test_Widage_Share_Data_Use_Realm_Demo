4cb7ab312fa5e2606e4fe502341b7a158567ec8e
lengxiang <csjlengxiang@gmail.com>
2017-02-21 18:14:23 +0800

    test move realm file to share place

--- testwidage/testwidage/ViewController.m
+++ testwidage/testwidage/ViewController.m
@@ -36,22 +36,58 @@
     
     NSLog(@"%@", pp);
     
-    
+//    [self setupOldRealm];
     [self setupRealm];
     
     [p save];
 }
 
-- (void)setupRealm {
+- (void)setupOldRealm {
     RLMRealmConfiguration * config = [RLMRealmConfiguration defaultConfiguration];
+    NSURL * oldPath = [[[config.fileURL URLByDeletingLastPathComponent]
+                        URLByAppendingPathComponent:@"SavingPlus"]
+                       URLByAppendingPathExtension:@"realm"];
+    
     NSURL *containerURL = [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:@"group.SavingPlusData"];
     containerURL = [containerURL URLByAppendingPathComponent:@"Library/Caches/"];
+    NSURL * newPath = [[[containerURL URLByDeletingLastPathComponent]
+                        URLByAppendingPathComponent:@"SavingPlus"]
+                       URLByAppendingPathExtension:@"realm"];
     
-    config.fileURL = [[[containerURL URLByDeletingLastPathComponent]
-                       URLByAppendingPathComponent:@"SavingPlus"]
-                      URLByAppendingPathExtension:@"realm"];
+    NSLog(@"old path %@", oldPath.path);
+    NSLog(@"new path %@", newPath.path);
     
-    NSLog(@"realm url: %@", config.fileURL);
+    config.fileURL = oldPath;
+    config.schemaVersion = 1;
+    config.migrationBlock = ^(RLMMigration *migration, uint64_t oldSchemaVersion) {
+        // do nothing
+    };
+    [RLMRealmConfiguration setDefaultConfiguration:config];
+}
+
+- (void)setupRealm {
+    RLMRealmConfiguration * config = [RLMRealmConfiguration defaultConfiguration];
+    NSURL * oldPath = [[[config.fileURL URLByDeletingLastPathComponent]
+                           URLByAppendingPathComponent:@"SavingPlus"]
+                          URLByAppendingPathExtension:@"realm"];
+    
+    NSURL *containerURL = [[NSFileManager defaultManager] containerURLForSecurityApplicationGroupIdentifier:@"group.SavingPlusData"];
+    containerURL = [containerURL URLByAppendingPathComponent:@"Library/Caches/"];
+    NSURL * newPath = [[[containerURL URLByDeletingLastPathComponent]
+                        URLByAppendingPathComponent:@"SavingPlus"]
+                       URLByAppendingPathExtension:@"realm"];
+    
+    NSLog(@"old path %@", oldPath.path);
+    NSLog(@"new path %@", newPath.path);
+    
+    
+    if ([[NSFileManager defaultManager] fileExistsAtPath:oldPath.path]) {
+        NSError * err;
+        [[NSFileManager defaultManager] moveItemAtURL:oldPath toURL:newPath error:&err];
+    }
+    
+    
+    config.fileURL = newPath;
     
     config.schemaVersion = 1;
     config.migrationBlock = ^(RLMMigration *migration, uint64_t oldSchemaVersion) {
